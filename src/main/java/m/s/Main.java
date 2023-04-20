de48d51133d7d74343e09eda55cb3248705dd979
Ethan Yin <yinssh1005@gmail.com>
2023-04-20 19:09:59 +0800

    added junit5 dependency

--- src/main/java/m/s/Main.java
+++ src/main/java/m/s/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void forTestging() {
+        System.out.println("tested");
+    }
 }
---
+++ src/test/java/m/s/MainTest.java
@@ -0,0 +1,12 @@
+package m.s;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void forTestging() {
+        Assertions.assertEquals(1, 1);
+    }
+}
